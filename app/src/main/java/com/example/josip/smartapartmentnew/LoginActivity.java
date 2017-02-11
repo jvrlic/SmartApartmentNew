39af3cfc96bde3d2aee20e611ae58f382eab2dad
Josip Vrlić <jvrlic@gmail.com>
2017-02-11 13:44:00 +0100

    Kostur s logiranjem

---
+++ app/src/main/java/com/example/josip/smartapartmentnew/LoginActivity.java
@@ -0,0 +1,372 @@
+package com.example.josip.smartapartmentnew;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.android.gms.tasks.Tasks;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+//import com.google.firebase.database.FirebaseError;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.R.id.message;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    //private Handler mUiHandler = new Handler();
+
+    //defining firebaseauth object
+    private FirebaseAuth mFirebaseAuth;
+
+    // UI references.
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private CheckBox mAutoLoginView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    private boolean mFinished = false;
+
+    private String mUserUid;
+    private DatabaseReference mDatabase;
+    private List<String> mApartmentIDs;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        SharedPreferences prefs = this.getSharedPreferences("PREF_PERSONAL_DATA", Context.MODE_PRIVATE);
+
+        mApartmentIDs = new ArrayList<>();
+
+        //initializing firebase auth object
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        // Set up the login form.
+        mEmailView = (EditText) findViewById(R.id.email);
+        mEmailView.setText(prefs.getString("email", null));
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+        mPasswordView.setText(prefs.getString("password", null));
+
+        mAutoLoginView = (CheckBox) findViewById(R.id.auto_sign_in_check_box);
+        mAutoLoginView.setChecked(prefs.getBoolean("autoLogin", false));
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+
+        if (mAutoLoginView.isChecked() == true)
+            mEmailSignInButton.callOnClick();
+    }
+
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString().trim();
+        String password = mPasswordView.getText().toString().trim();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+
+            mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //showProgress(false);
+                                saveLoginData();
+                                mDatabase = FirebaseDatabase.getInstance().getReference();
+                                mUserUid = task.getResult().getUser().getUid();
+                                Toast.makeText(LoginActivity.this, "Successfully sign in", Toast.LENGTH_SHORT).show();
+                            } else {
+                                showProgress(false);
+                                Toast.makeText(LoginActivity.this, "Registration Error", Toast.LENGTH_LONG).show();
+                                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                                mPasswordView.requestFocus();
+                            }
+                        }
+                    })
+                    .continueWithTask(new GetApartmentIDs())
+                    .continueWithTask(new GetApartmentDetails())
+                    .addOnCompleteListener(this, new OnCompleteListener<String>() {
+                                @Override
+                                public void onComplete(@NonNull Task<String> task) {
+                                    Log.d("DEBUG", "KRAJ: " + task.getResult());
+                                    showProgress(false);
+                                    if (task.getResult() == "OK")
+                                        finish();
+                                }
+                            });
+            Log.d("DEBUG", "DRUGO");
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+
+    private void saveLoginData() {
+        SharedPreferences prefs = this.getSharedPreferences("PREF_PERSONAL_DATA", Context.MODE_PRIVATE);
+
+        prefs.edit().putString("email", mEmailView.getText().toString().trim()).commit();
+        prefs.edit().putString("password", mPasswordView.getText().toString().trim()).commit();
+        prefs.edit().putBoolean("autoLogin", mAutoLoginView.isChecked()).commit();
+
+    }
+
+    private void saveApartmentData(String key, String value) {
+        SharedPreferences prefs = this.getSharedPreferences("PREF_PERSONAL_DATA", Context.MODE_PRIVATE);
+
+        prefs.edit().putString(key, value).commit();
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+
+    private class GetApartmentIDs implements Continuation<AuthResult, Task<String>> {
+        @Override
+        public Task<String> then(Task<AuthResult> task) {
+            final TaskCompletionSource<String> tcs = new TaskCompletionSource();
+
+            if (task.isSuccessful()) {
+                Log.d("DEBUG", mUserUid);
+
+                mDatabase.child("users")
+                        .child(mUserUid)
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                Log.d("DEBUG", "DOHVACAM APARTMANE ID");
+                                saveApartmentData("numberOfApartments", Long.toString(dataSnapshot.getChildrenCount()));
+                                for (DataSnapshot sn : dataSnapshot.getChildren()) {
+                                    mApartmentIDs.add(sn.getValue().toString());
+                                    saveApartmentData(sn.getKey(), sn.getValue().toString());
+                                }
+                                Log.d("DEBUG", "DOHVATIO APARTMANE ID");
+                                tcs.setResult("OK");
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                Log.d("DEBUG", "GRESKA DOHVACAM APARTMANE ID");
+                                tcs.setResult("KO");
+                            }
+                        });
+            } else {
+                tcs.setResult("KO");
+            }
+            return tcs.getTask();
+        }
+    }
+
+    private class GetApartmentDetails implements Continuation<String, Task<String>> {
+        @Override
+        public Task<String> then(Task<String> task) {
+            final TaskCompletionSource<String> tcs = new TaskCompletionSource();
+            Log.d("DEBUG", "NUMBER OF APARTMENTS: " + mApartmentIDs.size());
+
+            if (task.getResult() == "OK") {
+                for (String str : mApartmentIDs) {
+                    mDatabase.child(str)
+                            .addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    Log.d("DEBUG", "DOHVACAM DETALJE: ".concat(dataSnapshot.getKey()));
+                                    String name = dataSnapshot.child("name").getValue().toString();
+
+                                    saveApartmentData(dataSnapshot.getKey().concat("_name"), name);
+
+                                    saveApartmentData(dataSnapshot.getKey().concat("_climate_count"), Long.toString(dataSnapshot.child("Climates").getChildrenCount()));
+                                    int i = 1;
+                                    for (DataSnapshot ds : dataSnapshot.child("Climates").getChildren()) {
+                                        saveApartmentData(dataSnapshot.getKey().concat("_climate_name").concat(Integer.toString(i)), ds.getKey());
+                                        saveApartmentData(dataSnapshot.getKey().concat("_climate_id").concat(Integer.toString(i)), ds.getValue().toString());
+                                        i++;
+                                    }
+                                    saveApartmentData(dataSnapshot.getKey().concat("_door_count"), Long.toString(dataSnapshot.child("Doors").getChildrenCount()));
+                                    i = 1;
+                                    for (DataSnapshot ds : dataSnapshot.child("Doors").getChildren()) {
+                                        saveApartmentData(dataSnapshot.getKey().concat("_door_name").concat(Integer.toString(i)), ds.getKey());
+                                        saveApartmentData(dataSnapshot.getKey().concat("_door_id").concat(Integer.toString(i)), ds.getValue().toString());
+                                        i++;
+                                    }
+
+                                    if (dataSnapshot.getKey().equalsIgnoreCase(mApartmentIDs.get(mApartmentIDs.size() - 1))) {
+                                        Log.d("DEBUG", "GOTOVO DOHVACANJE DETALJA APARTMANA");
+                                        //showProgress(false);
+                                        Toast.makeText(LoginActivity.this, "Retreiving data finished", Toast.LENGTH_SHORT).show();
+                                        //finish();
+                                        tcs.setResult("OK");
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                    Log.d("DEBUG", "GRESKA DOHVACAM APARTMANE DETALJE");
+                                    tcs.setResult("KO");
+                                }
+                            });
+                }
+            } else {
+                tcs.setResult("KO");
+            }
+            return tcs.getTask();
+        }
+    }
+}
--- app/src/main/java/com/example/josip/smartapartmentnew/MainActivity.java
+++ app/src/main/java/com/example/josip/smartapartmentnew/MainActivity.java
@@ -1,22 +1,165 @@
 package com.example.josip.smartapartmentnew;
 
-import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Semaphore;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    private FirebaseUser mFirebaseUser;
+
+    private int mSelectedApartment;
+    private List<String> mApartmentIDs;
+    private List<List<String>> mClimateIDs;
+    private List<List<String>> mDoorIDs;
+    private Map<String,String> mNames;
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // do your stuff here after SecondActivity finished.
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mApartmentIDs = new ArrayList<String>();
+        mClimateIDs = new ArrayList<>();
+        mDoorIDs = new ArrayList<>();
+        mNames = new HashMap<>();
+
+
+
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+
+        int requestCode = 0;
+        if (mFirebaseUser == null) {
+           startActivityForResult(new Intent(this, LoginActivity.class), requestCode);
+        }
+
+        // kakvo je stanje s mFirebaseUser ako se startao novi activity
+
+        SharedPreferences prefs = this.getSharedPreferences("PREF_PERSONAL_DATA", Context.MODE_PRIVATE);
+
+        int count = Integer.parseInt(prefs.getString("numberOfApartments", "0"));
+        for (int i = 0; i < count; i++) {
+            String id = prefs.getString("apartment".concat(Integer.toString(i + 1)), "");
+            mApartmentIDs.add(id);
+            mNames.put(id, prefs.getString(id.concat("_name"), ""));
+
+            int nClimates = Integer.parseInt(prefs.getString(id.concat("_climate_count") , "0"));
+            List<String> climatesTemp = new ArrayList<>();
+            for (int j = 1; j <= nClimates; j++) {
+                String idClimate = prefs.getString(id.concat("_climate_id").concat(Integer.toString(j)), "climate_".concat(Integer.toString(i * 10 + j)));
+                climatesTemp.add(idClimate);
+                mNames.put(idClimate, prefs.getString(id.concat("_climate_name").concat(Integer.toString(j)), "climate_".concat(Integer.toString(i * 10 + j))));
+            }
+            mClimateIDs.add(climatesTemp);
+
+            int nDoors = Integer.parseInt(prefs.getString(id.concat("_door_count") , "0"));
+            List<String> doorsTemp = new ArrayList<>();
+            for (int j = 1; j <= nDoors; j++) {
+                String idDoor = prefs.getString(id.concat("_door_id").concat(Integer.toString(j)), "door_".concat(Integer.toString(i * 10 + j)));
+                doorsTemp.add(idDoor);
+                mNames.put(idDoor, prefs.getString(id.concat("_door_name").concat(Integer.toString(j)), "door_".concat(Integer.toString(i * 10 + j))));
+            }
+            mDoorIDs.add(doorsTemp);
+        }
+
+        mSelectedApartment = 0;
+
+        setTitle(mNames.get(mApartmentIDs.get(0)));
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,10 +171,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        for (String str : mApartmentIDs) {
+            menu.add(mNames.get(str));
+        }
+
         return true;
     }
 
@@ -42,11 +192,90 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sign_out) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+
+            return 1 + mClimateIDs.get(mSelectedApartment).size() + mDoorIDs.get(mSelectedApartment).size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Overview";
+                case 1:
+                    return "SECTION B";
+                case 2:
+                    return "SECTION C";
+                case 3:
+                    return "SECTION D";
+            }
+            return null;
+        }
     }
 }
